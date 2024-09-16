3a0653f18996671f0070c8860ccd436e258d681e
SilverDragon <kagangtuya@foxmail.com>
2024-09-16 18:12:33 +0800

    添加：将HTML代码输出到剪贴板
    输出内容缺失的补救方案

--- Simulacrum.py
+++ Simulacrum.py
@@ -1,6 +1,7 @@
 from tkinter import Tk, Button, filedialog
 import os
 import re
+import pyperclip
 
 import win32clipboard as winclip
 import win32con
@@ -15,8 +16,10 @@ from HTMLTagTraverse import get_page_default_name
 WINDOW: Tk
 
 #——————————————————————————————————————————
+# ——————————————————————————————————————————
 
-#显示剪贴板源数据
+# 显示剪贴板源数据
+
 def clipboard_origin():
     origin = hcp.DumpHtml()
     if origin != "":
@@ -33,6 +36,8 @@ def clipboard_origin():
             print("剪贴板内容为空")
 
 # 将果园文本转化为不全书html
+
+
 def html_to_notall():
     origin = hcp.DumpHtml()
     output = morph_notallbook_chm_html(origin)
@@ -41,17 +46,21 @@ def html_to_notall():
     hcp.PutHtml(output)
 
 # 处理果园复制下来的文本
+
+
 def goddess_process():
     origin = hcp.DumpHtml()
-    output = clean_trash_format(origin,"goddess")
+    output = clean_trash_format(origin, "goddess")
     print(output)
     print("完成！")
     hcp.PutHtml(output)
 
 # 处理RTF复制下来的文本
+
+
 def rtf_process():
-    origin = hcp.DumpHtml().replace("\n","")
-    output = clean_trash_format(origin,"rtf")
+    origin = hcp.DumpHtml().replace("\n", "")
+    output = clean_trash_format(origin, "rtf")
     print(output)
     print("完成！")
     hcp.PutHtml(output)
@@ -66,41 +75,46 @@ def html_to_bbcode():
     try:
         winclip.OpenClipboard()
         winclip.EmptyClipboard()
-        winclip.SetClipboardData(win32con.CF_UNICODETEXT,output)
+        winclip.SetClipboardData(win32con.CF_UNICODETEXT, output)
     finally:
         winclip.CloseClipboard()
 
-#————————————————————
+# ————————————————————
+
+
 def save_with_template_html():
     # 保存，但以读取模板并填写的格式保存
     origin = hcp.DumpHtml()
     if origin != "":
         page_default_name = get_page_default_name(origin)
         template: str = ""
-        with open("template/Empty.htm","r",encoding="GBK") as f:
+        with open("template/Empty.htm", "r", encoding="GBK") as f:
             template = f.read()
         if not os.path.exists("output"):
             os.makedirs("output")
-        #打开窗口让用户选择
-        user_path = filedialog.asksaveasfile(title="请选择保存位置", initialdir="./output/", initialfile=page_default_name+".htm", filetypes=[("不全书HTML文件", ".htm .html")], defaultextension=".htm")
+        # 打开窗口让用户选择
+        user_path = filedialog.asksaveasfile(title="请选择保存位置", initialdir="./output/", initialfile=page_default_name +
+                                             ".htm", filetypes=[("不全书HTML文件", ".htm .html")], defaultextension=".htm")
         if user_path == None:
             print("已取消保存")
             return
         print('已保存至：', user_path)
         output_name = os.path.splitext(os.path.basename(user_path.name))[0]
-        with open(user_path.name,"w",encoding="GBK") as f:
-            f.write(template.replace("{{内容}}",origin).replace("{{标题}}",output_name))
+        with open(user_path.name, "w", encoding="GBK") as f:
+            f.write(template.replace("{{内容}}", origin).replace(
+                "{{标题}}", output_name))
         print("已保存！")
     else:
         print("剪贴板为空/不是HTML！")
-    
+
+
 def save():
     # 保存
     origin = hcp.DumpHtml()
     if origin != "":
         if not os.path.exists("output"):
             os.makedirs("output")
-        with open("output/output.txt","w",encoding="UTF-8") as f:
+        with open("output/output.txt", "w", encoding="UTF-8") as f:
             f.write(origin)
         print("已保存！")
     else:
@@ -113,18 +127,20 @@ def save():
         if text != "":
             if not os.path.exists("output"):
                 os.makedirs("output")
-            with open("output/output.txt","w",encoding="UTF-8") as f:
+            with open("output/output.txt", "w", encoding="UTF-8") as f:
                 f.write(text)
                 print("已保存！")
         else:
             print("剪贴板为空/无法解析！")
 
 # 将文本转化为dnd样式的首行加粗间隔表格table
+
+
 def make_table():
     origin = hcp.DumpHtml()
     if origin != "":
-        output = origin.replace("<br>","\n")
-        output = re.compile(r'<[^>]+>',re.S).sub("",output)
+        output = origin.replace("<br>", "\n")
+        output = re.compile(r'<[^>]+>', re.S).sub("", output)
     else:
         winclip.OpenClipboard(0)
         src = winclip.GetClipboardData()
@@ -136,19 +152,44 @@ def make_table():
     print("完成！")
     hcp.PutHtml(output)
 
+# 将HTML代码以HTML格式输出到剪贴板
+
+
+def html_to_htmlcode():
+    origin = hcp.DumpHtml()
+    if origin != "":
+        # 保留HTML标签，直接输出到剪贴板
+        print(origin)
+        pyperclip.copy(origin)
+        print("已将HTML代码输出到剪贴板")
+    else:
+        try:
+            winclip.OpenClipboard()
+            text = winclip.GetClipboardData(win32con.CF_UNICODETEXT)
+        finally:
+            winclip.CloseClipboard()
+        if text != "":
+            print(text)
+            pyperclip.copy(text)
+            print("已将HTML代码输出到剪贴板")
+        else:
+            print("剪贴板内容为空或不是HTML")
+
 
 # UI
 if __name__ == "__main__":
     WINDOW = Tk()
-    def a_button(text: str,command):
-        Button(WINDOW, text = text, command = command).pack()
 
-    a_button("显示当前剪贴板源数据",clipboard_origin)
-    a_button("处理果园复制的富文本",goddess_process)
-    a_button("处理DOC/RTF复制的富文本",rtf_process)
-    a_button("html -> 果园BBcode",html_to_bbcode)
-    a_button("html -> 不全书格式",html_to_notall)
-    a_button(f"保存为htm文件",save_with_template_html)
-    a_button(f"保存为txt文件",save)
-    a_button("用已复制文本制作表格(用|分隔）",make_table)
+    def a_button(text: str, command):
+        Button(WINDOW, text=text, command=command).pack()
+
+    a_button("显示当前剪贴板源数据", clipboard_origin)
+    a_button("将HTML代码输出到剪贴板", html_to_htmlcode)
+    a_button("处理果园复制的富文本", goddess_process)
+    a_button("处理DOC/RTF复制的富文本", rtf_process)
+    a_button("html -> 果园BBcode", html_to_bbcode)
+    a_button("html -> 不全书格式", html_to_notall)
+    a_button(f"保存为htm文件", save_with_template_html)
+    a_button(f"保存为txt文件", save)
+    a_button("用已复制文本制作表格(用|分隔）", make_table)
     WINDOW.mainloop()
